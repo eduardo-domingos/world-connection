40fad8432506acc65c8a93ce58b60cf8a3a2e840
Eduardo Domingos <eduardoaparecido461@gmail.com>
2022-11-03 21:19:01 -0300

    Implementação do Padrão de Projeto Singleton na classe de conexão

--- App/Core/Connection.php
+++ App/Core/Connection.php
@@ -9,24 +9,42 @@ class Connection
 {
     
     /**
-     * Retorna conexão com o banco
-     * @return PDO
+     * Contém a instância da conexão
+     * @var PDO 
      */
-    public static function getDb(): PDO
+    protected static PDO $db;
+    
+    private function __construct(){}
+    private function __clone(){}
+    public function __wakeup(){}
+
+    /**
+     * Se não existe a conexão com o banco, cria
+     * Se existir a conexão com o banco, retorna a existente
+     */
+    public static function getDb()
     {
+
         try{
+
+            if(empty(self::$db)){
             
-            $options = [
-                PDO::ATTR_PERSISTENT => true,
-                PDO::ATTR_ERRMODE   => PDO::ERRMODE_EXCEPTION
-            ];
-            
-            return (new PDO('mysql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT').';charset='.getenv('DB_CHAR').';dbname='.getenv('DB_BASE'),  getenv('DB_USER'), getenv('DB_PASS'), $options));
+                $options = [
+                    PDO::ATTR_PERSISTENT => true,
+                    PDO::ATTR_ERRMODE   => PDO::ERRMODE_EXCEPTION
+                ];
+                
+                self::$db = (new PDO('mysql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT').';charset='.getenv('DB_CHAR').';dbname='.getenv('DB_BASE'),  getenv('DB_USER'), getenv('DB_PASS'), $options));
+
+            }
+
+            return self::$db;
             
         } catch (PDOException $error) {
             phpError($error->getCode(),  $error->getMessage(), $error->getFile(), $error->getLine());
             exit();
         }
+
     }
     
 }
